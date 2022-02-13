bd82f2151a5d1d9b80eb8cee7333fbff54d72f5b
beomso0 <bumsookim98@naver.com>
2022-02-14 02:59:05 +0900

    .

--- st_test.py
+++ st_test.py
@@ -1,4 +1,5 @@
 # import libraries
+from asyncore import close_all
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -10,11 +11,18 @@ from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error,
 import math
 import datetime
 
-# declare global variables
+# set overall layout
 st.set_page_config(layout='wide')
-file_uploaded = 0
-preprocess_done = 0
-predict_done = 0 
+
+# declare session states
+if 'file_uploaded' not in st.session_state:
+     st.session_state.file_uploaded = 0
+if 'preprocess_done' not in st.session_state:
+     st.session_state.preprocess_done = 0
+if 'predict_done' not in st.session_state:
+     st.session_state.predict_done = 0
+
+# dataframe 생성
 if 'df_to_predict' not in st.session_state:
      st.session_state.df_to_predict = pd.DataFrame(columns=[
           'pgm_input',
@@ -22,6 +30,7 @@ if 'df_to_predict' not in st.session_state:
           'weather_input',
           'weekday_input',
           'brand_input',
+          'price_input',
           'expression_input',
           'date_input',
           'time_input',
@@ -55,7 +64,7 @@ if uploaded_file is not None:
      # Can be used wherever a "file-like" object is accepted:
      dataframe = pd.read_csv(uploaded_file)
      st.write(dataframe)
-     file_uploaded = 1
+     st.session_state.file_uploaded = 1
 
 
 @st.cache
@@ -73,11 +82,11 @@ def convert_df(df):
      # IMPORTANT: Cache the conversion to prevent computation on every rerun
      return df.to_csv().encode('utf-8')
 
-if (file_uploaded == 1) & (preprocess_done == 1):
+if (st.session_state.file_uploaded == 1) & (st.session_state.preprocess_done == 1):
      output = make_pred(model,dataframe)
-     predict_done = 1
+     st.session_state.predict_done = 1
 
-if predict_done == 1:   
+if st.session_state.predict_done == 1:   
      st.header('예측완료!')
      st.write(output)
 
@@ -90,41 +99,14 @@ if predict_done == 1:
           mime='text/csv',
      )
 
-def update_df(
-          pgm_input,
-          showhost_input,
-          weather_input,
-          weekday_input,
-          brand_input,
-          expression_input,
-          date_input,
-          time_input,
-          duration_input,
-          prd_num_input,
-          live_input,
-          holiday_input
-          ):
-     st.session_state.df_to_predict.append({
-               'pgm_input':pgm_input,
-               'showhost_input':showhost_input,
-               'weather_input':weather_input,
-               'weekday_input':weekday_input,
-               'brand_input':brand_input,
-               'expression_input':expression_input,
-               'date_input':date_input,
-               'time_input':time_input,
-               'duration_input':duration_input,
-               'prd_num_input':prd_num_input,
-               'live_input':live_input,
-               'holiday_input':holiday_input
-     },ignore_index=True)
+# sidebar form
+with st.sidebar.form(key='columns_in_form'):
+          col1, col2 = st.columns(2)
+          with col1:    
+               save = st.form_submit_button('저장')                    
+          with col2:
+               predict = st.form_submit_button('예측')
 
-
-col1, col2 = st.columns([1,3])
-
-with col1:
-#test
-     with st.form(key='columns_in_form'):
           pgm_input = st.selectbox(
                'PGM명을 선택하세요', ref['pgm_ref']
           )
@@ -139,6 +121,9 @@ with col1:
           )
           brand_input = st.multiselect(
                '판매 브랜드를 모두 선택하세요', ref['brand_ref']
+          )
+          price_input = st.number_input(
+               '상품가격을 모두 입력하세요', 0
           )
           expression_input = st.multiselect(
                '사용 예정인 한정표현을 모두 선택하세요', ref['expression_ref']
@@ -162,65 +147,27 @@ with col1:
                '주말 혹은 공휴일인가요?',
           )    
 
-          # 제출
-          submitted = st.form_submit_button('Submit',on_click=update_df,args=[
-               pgm_input,
-               showhost_input,
-               weather_input,
-               weekday_input,
-               brand_input,
-               expression_input,
-               date_input,
-               time_input,
-               duration_input,
-               prd_num_input,
-               live_input,
-               holiday_input
-          ])
+          # submit
+          submitted = st.form_submit_button('Submit')
+          # update dataframe
+          if submitted:
+               st.session_state.df_to_predict = st.session_state.df_to_predict.append({
+                                                                           'pgm_input':pgm_input,
+                                                                           'showhost_input':showhost_input,
+                                                                           'weather_input':weather_input,
+                                                                           'weekday_input':weekday_input,
+                                                                           'brand_input':brand_input,
+                                                                           'expression_input':expression_input,
+                                                                           'date_input':date_input,
+                                                                           'time_input':time_input,
+                                                                           'duration_input':duration_input,
+                                                                           'prd_num_input':prd_num_input,
+                                                                           'live_input':live_input,
+                                                                           'holiday_input':holiday_input,
+                                                                           'price_input':price_input
+                                                                 },ignore_index=True)
 
-with col2:
-     st.write(st.session_state.df_to_predict)
-
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-# @st.cache()
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
-
-# # Create a text element and let the reader know the data is loading.
-# data_load_state = st.text('Loading data...')
-# # Load 10,000 rows of data into the dataframe.
-# data = load_data(10000)
-# # Notify the reader that the data was successfully loaded.
-# data_load_state.text('Done! (using st.cache)')
-
-# st.subheader('Raw data')
-# st.write(data)
-
-# st.subheader('Number of pickups by hour')
-# hist_values = np.histogram(
-#     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-# st.bar_chart(hist_values)
-
-# st.subheader('Map of all pickups')
-# st.map(data)
-
-# hour_to_filter = 17
-# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-# st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-# st.map(filtered_data)
-
-# hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-
-# #test
-# if st.checkbox('Show raw data'):
-#     st.subheader('Raw data')
-#     st.write(data)
+# show dataframe
+st.dataframe(st.session_state.df_to_predict.style.format(na_rep='입력없음',precision=0))
 
 
